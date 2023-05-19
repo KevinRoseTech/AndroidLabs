00d56a9f5820b549ea8b89143690f93d4dbf7530
Kevin <kevinjodavid@outlook.com>
2023-05-19 09:46:25 -0600

    Lab2: Basic Toast function with undo added, does not display correct on/off status

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +25,23 @@ public class MainActivity extends AppCompatActivity {
         EditText et = findViewById(R.id.editText);
         String tt = getResources().getString(R.string.toast_message);
         int duration = Toast.LENGTH_SHORT;
+        int durationSB = Snackbar.LENGTH_SHORT;
         Toast toast = Toast.makeText(getApplicationContext(), tt, duration);
+        String chkOn = getResources().getString(R.string.checkbox_on);
+        String chkOff = getResources().getString(R.string.checkbox_off);
+        String chkUnknown = getResources().getString(R.string.checkbox_unknown);
+        String chkMessage = getResources().getString(R.string.checkbox_status);
+        CheckBox cb = findViewById(R.id.checkBox);
+        String chkStatus;
+
+
+
 
         // Chains the actions in one button click listen
         btn.setOnClickListener((click) -> {tv.setText(et.getText());toast.show();});
+        cb.setOnCheckedChangeListener(((buttonView, isChecked) -> {Snackbar.make(cb,chkMessage + chkOn, durationSB).setAction("Undo", click->cb.setChecked( !true )).show();}));
 
 
     }
+
 }
