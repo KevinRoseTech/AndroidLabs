e694aa2d50c5b2aadca8a68b2249887c385b656b
Kevin David <107796703+MarmotMoment@users.noreply.github.com>
2023-06-01 13:48:44 -0600

    Further work

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btn = findViewById(R.id.button);
+        EditText et = findViewById(R.id.editText);
+
+        Intent nextActivity = new Intent (this, NameActivity.class);
+        View.OnClickListener( click -> startActivityForResult(nextActivity, int code));
+
+
+        }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences prefs = getSharedPreferences(String fileName, int mode);
+
     }
+
+}
 }
