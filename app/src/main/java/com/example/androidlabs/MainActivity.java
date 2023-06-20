0c753e781be7a55cc09fecdf1498c7f585d44a0d
Kevin <kevinjodavid@outlook.com>
2023-06-20 11:10:36 -0600

    Lab6: INTERNET NOW WORKING <uses-permission android:name="android.permission.INTERNET" /> Need to fix the progress bar, need to fix the updates in general, but finally it parses json properly....

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.androidlabs;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONObject;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -15,70 +23,70 @@ import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Begin thread
-        CatImages req = new CatImages();
-        req.execute("http://api.openweathermap.org/data/2.5/uvi?appid=7e943c97096a9784391a981c4d878b22&lat=45.348945&lon=-75.759389"
-        );
+
+        imageView = findViewById(R.id.imageView); // Replace with your ImageView's id.
+        progressBar = findViewById(R.id.progressBar); // Replace with your ProgressBar's id.
+
+        new DownloadImageTask().execute("https://cataas.com/cat?json=true");
     }
 
-    class CatImages extends AsyncTask< String, Integer, String> {
-
-        public String doInBackground(String ... args) {
-            int x = 0;
-            while (x == 0){
+    private class DownloadImageTask extends AsyncTask<String, Integer, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            Bitmap bitmap = null;
             try {
-            //create a URL object of what server to contact:
-            URL url = new URL(args[0]);
-            //open the connection
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            //wait for data:
-            InputStream response = urlConnection.getInputStream();
+                // Download JSON.
+                URL url = new URL(urls[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream response = urlConnection.getInputStream();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null)
-                {
-                    sb.append(line + "\n");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line;
+                int progress = 0;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
+
+                    // This is a dummy progress update for the example. You'd have to calculate the actual progress based on the data you have.
+                    progress += 10;
+                    publishProgress(progress);
                 }
                 String result = sb.toString();
+
+                // Parse JSON.
+                JSONObject catObject = new JSONObject(result);
+                String imageUrl = "https://cataas.com" + catObject.getString("url");
+
+                // Download image.
+                InputStream in = new URL(imageUrl).openStream();
+                bitmap = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
             }
-            catch (Exception e)
-            {}
-            //infinite loop
-
-                //TODO: Implement cat
-                //Progress bar can be thought of as %
-                //i represnts progress % from 0-100
-                for (int i = 0; i < 100; i++) {
-                    try {
-                        publishProgress(i);
-                        Thread.sleep(30);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-
-            }
-            return "donetest";
+            return bitmap;
         }
-        public void onProgressUpdate(Integer ... args)
-        {
-            super.onProgressUpdate(args);
 
-            ProgressBar progressBar = findViewById(R.id.progressBar);
-            progressBar.setProgress(args[0]);
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            // Update the progress bar.
+            progressBar.setProgress(values[0]);
         }
-        //Type3
-        public void onPostExecute(String fromDoInBackground)
-        {
-            Log.i("HTTP", fromDoInBackground);
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            // Hide the progress bar when finished.
+            progressBar.setVisibility(View.GONE);
+            // Set the image to the ImageView.
+            imageView.setImageBitmap(result);
         }
     }
-
 }
