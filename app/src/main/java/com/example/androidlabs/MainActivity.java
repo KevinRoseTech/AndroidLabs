f89c8c532e65590c81ff1851444549e784cdbbcf
Kevin <kevinjodavid@outlook.com>
2023-07-05 13:03:37 -0600

    Lab8: XML structuring

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,6 +1,16 @@
 package com.example.androidlabs;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +19,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //This gets the toolbar from the layout:
+        Toolbar tBar = findViewById(R.id.toolbar);
+
+        //This loads the toolbar, which calls onCreateOptionsMenu below:
+        setSupportActionBar(tBar);
+        //Toolbar myToolbar = (Toolbar)findViewById(R.id.toolbar);
+        //setSupportActionBar(myToolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return true;
+    }
+
+
+    private void setSupportActionBar(Toolbar tBar) {
+    }
+
+    //private void setSupportActionBar(Toolbar myToolbar) {
+    //}
 }
