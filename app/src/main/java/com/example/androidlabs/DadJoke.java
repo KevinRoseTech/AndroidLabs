a88bd91c16c17eeed2854968082c5b25017665b4
Kevin David <107796703+MarmotMoment@users.noreply.github.com>
2023-07-06 16:17:40 -0600

    Completed: //TODO: Localize to french(?) Remaining todos:

--- app/src/main/java/com/example/androidlabs/DadJoke.java
+++ app/src/main/java/com/example/androidlabs/DadJoke.java
@@ -1,14 +1,11 @@
 package com.example.androidlabs;
 
 import android.os.Bundle;
-
 public class DadJoke extends BaseActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dad_joke);
-
         setupToolbar();
         setupNavigationDrawer();
     }
